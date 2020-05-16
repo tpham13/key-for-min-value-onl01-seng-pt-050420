7180cd757f05f82b6d4c521d23ecd7c20d480454
Thanh Pham <tpham0123@gmail.com>
2020-05-16 19:01:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,17 @@
 
 fruit_inv = {apples: 20, kiwis: 55, tomatoes: 13}
 
-def key_for_min_value(hash) 
-  num = v
+def key_for_min_value(name_has)
+  min_key, min_val =[nil, nil]
+  name_hash.each do |key, value|
+    if min_val == nil || value < min_val
+      min_val = value
+      min_key = key
+      
+    end 
+  end 
+  min_key
+end 
   
   
   end 
